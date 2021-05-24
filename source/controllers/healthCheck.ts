f17f75b7f04329762651e29e34efae6d5891a844
Pablo Rivera <pablorich1@hotmail.com>
2021-05-24 16:54:14 -0500

    fixes for #5 Description must be read from package.json Must subscribe only to ON_MESSAGE event

--- source/controllers/healthCheck.ts
+++ source/controllers/healthCheck.ts
@@ -1,13 +1,16 @@
 import { Request, Response, NextFunction } from 'express';
 import logging from '../config/logging';
+const { description } = require('../../package.json');
 
 const NAMESPACE = 'Health Check Controller';
 
 const healthCheck = (req: Request, res: Response, _next: NextFunction) => {
-    logging.info(NAMESPACE, 'Sample health check route called.');
+    logging.info(NAMESPACE, 'Health check route called.');
 
     return res.status(200).json({
-        status: 'OK'
+        status: 'OK',
+        description: description,
+        subscriptions: ['ON_MESSAGE']
     });
 };
 
