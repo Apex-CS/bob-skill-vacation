a14824fa9bb28cfded17b26971ea53550be06f89
Pablo Rivera <pablorich1@hotmail.com>
2021-05-25 12:05:37 -0500

    fetching excel sheet from onedrive with help of a package

--- source/controllers/message.ts
+++ source/controllers/message.ts
@@ -1,14 +1,19 @@
 import { Request, Response, NextFunction } from 'express';
 import logging from '../config/logging';
+const fetchSheet = require('@flumens/fetch-onedrive-excel');
+
+const file = '01MBSI4KQ4IMJ2O2WHS5E2PQIUQ3DYQWMJ';
+const sheet = 'Hoja1';
 
 const NAMESPACE = 'Message Controller';
 
-const message = (req: Request, res: Response, _next: NextFunction) => {
+const message = async (req: Request, res: Response, _next: NextFunction) => {
     logging.info(NAMESPACE, 'Message route called.');
-
+    let hoja = await fetchSheet({ file, sheet });
+    let pto = hoja.find((user: { mail: string; PTO: number }) => user.mail == req.body.email).PTO;
     return res.status(200).json({
         type: 'message',
-        message: 'YOU HAVE 0 PTO DAYS LEFT'
+        message: 'YOU HAVE ' + pto + ' PTO DAYS LEFT'
     });
 };
 
